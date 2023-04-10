13e2239195ca994a6e063d77588eba8c48cd4ccd
sartajsolaiman <ssdplugin@gmail.com>
2023-04-10 17:27:47 +0600

    tried to pass frontend data to backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Home from './pages/Home';
 import Create_poll from './pages/Create_poll';
 import Voting_page from './pages/Voting_page';
@@ -8,8 +10,33 @@ import Error from './pages/Error';
 import Navbar from './components/Navbar';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
+import { useState } from 'react';
 
 function App() {
+  const [firstName, setfirstName] = useState('')
+  const [lastName, setlastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function registerUser(event) {
+    event.preventDefault()
+     fetch('http://localhost:1337/register' , {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password
+      }),
+     }) 
+
+     const data = await Response.JSON()
+     console.log(data)
+  }
+
   return (
       <BrowserRouter> 
         
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,42 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 
 const Signup = () => {
+
+  const [firstName, setfirstName] = useState('')
+  const [lastName, setlastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const formInfo = {
+    username : "ajfksjf",
+    password : "sifdhisjfs"
+  }
+
+  useEffect(() => {
+    //event.preventDefault()
+     fetch('http://localhost:1337/register' , {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        //firstName,
+        //lastName,
+        //email,
+        //password
+        formInfo
+      }),
+     }) 
+     .then(res => res.json())
+     .then(firstName => setfirstName(firstName))
+     .then(lastName => setlastName(lastName))
+     .then(email => setEmail(email))
+     .then(password => setPassword(password))
+
+     //console.log(data)
+  },[])
+
   return (
     <div class="container bg-light my-5 d-flex justify-content-center">
       <form class="p-3 w-50">
@@ -8,6 +44,8 @@ const Signup = () => {
         <div className="mb-3">
           <label for="firstName" class="form-label fw-bold">First name</label>
           <input
+            value={firstName}
+            onChange={(e) => setfirstName(e.target.value)}
             type="text"
             id="firstName"
             className="form-control"
@@ -17,11 +55,12 @@ const Signup = () => {
         </div>
         <div className="mb-3">
           <label for="lastName" class="form-label fw-bold">Last name</label>
-          <input type="text" id="lastName" className="form-control" placeholder="Last name" />
+          <input value={lastName} onChange={(e) => setlastName(e.target.value)} type="text" id="lastName" className="form-control" placeholder="Last name" />
         </div>
         <div className="mb-3">
           <label for="email" class="form-label fw-bold">Email address</label>
           <input
+            value={email} onChange={(e) => setEmail(e.target.value)}
             type="email"
             id="email"
             className="form-control"
@@ -31,6 +70,7 @@ const Signup = () => {
         <div className="mb-3">
           <label for="password" class="form-label fw-bold">Password</label>
           <input
+            value={password} onChange={(e) => setPassword(e.target.value)}
             type="password"
             id="password"
             className="form-control"
