5d036682beb9e70f36ae4e40c6e0efea204a9b1e
RizwanCSEDU25 <rizwanhrizvi@gmail.com>
2023-04-10 20:45:12 +0600

    implemented signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,80 +1,85 @@
-import React from 'react'
-import { useState,useEffect } from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [firstName, setfirstName] = useState('')
-  const [lastName, setlastName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const formInfo = {
-    username : "ajfksjf",
-    password : "sifdhisjfs"
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
   }
 
-  useEffect(() => {
-    //event.preventDefault()
-     fetch('http://localhost:1337/register' , {
+  const handleLastNameChange = (e) => {
+    setLirstName(e.target.value);
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleSubmitChange = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:3000/api/auth/register' , {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        //firstName,
-        //lastName,
-        //email,
-        //password
-        formInfo
+        firstName,
+        lastName,
+        email,
+        password
       }),
      }) 
-     .then(res => res.json())
-     .then(firstName => setfirstName(firstName))
-     .then(lastName => setlastName(lastName))
-     .then(email => setEmail(email))
-     .then(password => setPassword(password))
-
-     //console.log(data)
-  },[])
-
+     const data = await response.json()
+     console.log(data)
+     
+  }
   return (
     <div class="container bg-light my-5 d-flex justify-content-center">
-      <form class="p-3 w-50">
+      <form class="p-3 w-50" onSubmit={handleSubmitChange}>
         <h1 className='d-flex justify-content-center'>Sign Up</h1>
         <div className="mb-3">
           <label for="firstName" class="form-label fw-bold">First name</label>
           <input
-            value={firstName}
-            onChange={(e) => setfirstName(e.target.value)}
             type="text"
             id="firstName"
             className="form-control"
             placeholder="First name"
+            value={firstName}
+            onChange={handleFirstNameChange}
             required
           />
         </div>
         <div className="mb-3">
           <label for="lastName" class="form-label fw-bold">Last name</label>
-          <input value={lastName} onChange={(e) => setlastName(e.target.value)} type="text" id="lastName" className="form-control" placeholder="Last name" />
+          <input type="text" id="lastName" className="form-control" placeholder="Last name" value={lastName} onChange={handleLastNameChange} />
         </div>
         <div className="mb-3">
           <label for="email" class="form-label fw-bold">Email address</label>
           <input
-            value={email} onChange={(e) => setEmail(e.target.value)}
             type="email"
             id="email"
             className="form-control"
             placeholder="Enter email"
+            value={email}
+            onChange={handleEmailChange}
           />
         </div>
         <div className="mb-3">
           <label for="password" class="form-label fw-bold">Password</label>
           <input
-            value={password} onChange={(e) => setPassword(e.target.value)}
             type="password"
             id="password"
             className="form-control"
             placeholder="Enter password"
+            value={password}
+            onChange={handlePasswordChange}
           />
         </div>
         <div className="d-grid">
