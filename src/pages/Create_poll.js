27196d208ed9bcfb8951a69a5c9f69d0caa0f8e9
sartajsolaiman <ssdplugin@gmail.com>
2023-04-12 21:45:37 +0600

    added voter fields

--- src/pages/Create_poll.js
+++ src/pages/Create_poll.js
@@ -4,6 +4,18 @@ const Create_poll = () => {
   const [title, setTitle] = useState("");
   const [starttime, startTime] = useState("");
   const [endtime, endTime] = useState("");
+  const [votermail, setVoterMail] = useState("");
+  const [voterid, setVoterID] = useState("");
+  const [questions, setQuestions] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    for (let i = 0; i < 1; i++) {
+      setCounter(counter + 1);
+    }
+    //console.log(counter);
+  };
+
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
@@ -17,11 +29,25 @@ const Create_poll = () => {
     endTime(e.target.value);
   }
 
+  const handleVotermailChange = (e) => {
+    setVoterMail(e.target.value);
+  }
+
+  const handleVoteridChange = (e) => {
+    setVoterID(e.target.value);
+  }
+
+  const handleQuestionChange = (e) => {
+    setQuestions(e.target.value);
+  }
+
   const handleSubmitChange = (e) => {
     let userInfo = {
       title: title,
       startTime: starttime,
-      endTime: endtime
+      endTime: endtime,
+      voterMail: votermail,
+      Questions: questions
     }
     console.log(userInfo)
     e.preventDefault();
@@ -51,6 +77,33 @@ const Create_poll = () => {
               <input type="datetime-local" id="end" class="form-control" value={endtime} onChange={handleEndTimeChange} required/>
               </div>
             </div>
+
+            <div class = "row mb-3">
+            
+            <div class = "col">
+              <button style={{ width: "150px", height: "50px",}} class="btn btn-lg btn-dark" onClick={handleClick}>Add Voter</button>
+
+              {Array.from(Array(counter)).map((c, index) => {
+                return <div>
+                  <label for="votermail" class="form-label fw-bold">Email </label>
+                  <input type="text" id="votermail" class="form-control" placeholder='Email' value={votermail} onChange={handleVotermailChange} required/>
+                  <div class="col">
+                    <label for="voterid" class="form-label fw-bold">Voter ID </label>
+                    <input type="text" id="voterid" class="form-control" placeholder='ID' value={voterid} onChange={handleVoteridChange} required/>
+                  </div>
+
+                  
+                   </div>;
+              })}
+            </div>
+            </div>
+            
+            <div class="mb-3">
+              <label for="questions" class="form-label fw-bold">Questions </label>
+              <input type="text" id="questions" class="form-control" placeholder='Questions' value={title} onChange={handleQuestionChange} required/>
+            </div>
+
+
 
             <div class="mb-3 pt-2">
               <button class="btn btn-lg btn-dark">Continue</button>
