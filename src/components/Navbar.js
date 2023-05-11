7e17c36a437348656c5f5307b59146f692597cad
RizwanCSEDU25 <rizwanhrizvi@gmail.com>
2023-05-12 01:42:33 +0600

    tets

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -126,7 +126,7 @@ const Navbar = () => {
                       </ul>
                       <ul class="navbar-nav ms-auto">
                       <div class="collapse navbar-collapse" id="navbarSupportedContent" style= {{paddingRight: "0.25rem"}}>
-                      <li class="nav-item"><button type="button" class="btn btn-outline-light" onClick={handleLogout}>Logout</button></li>
+                      <li class="nav-item"><a href='/signin'><button type="button" class="btn btn-outline-light" onClick={handleLogout}>Logout</button></a></li>
                       </div>
                       {/* <li class="nav-item"><button class="button" onClick={handleLogout}>Log out</button></li> */}
                       </ul>
